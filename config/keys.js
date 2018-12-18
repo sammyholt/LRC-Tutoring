f00c073142e83a49d78b923da9968647d92e04dc
Sammy Holt <samuel.l.holt@gmail.com>
2018-12-18 11:51:03 -0800

    Backend api complete

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://admin:admin1!@ds237574.mlab.com:37574/devconnector-network'
+    'mongodb://admin:admin1!@ds237574.mlab.com:37574/devconnector-network',
+  secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -18,6 +24,12 @@ mongoose
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => res.send('Hello'));
 
