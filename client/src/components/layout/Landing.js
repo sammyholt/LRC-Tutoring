2c84713ca1b371586195a5e7d896b78144291ad1
Sammy Holt <samuel.l.holt@gmail.com>
2019-03-20 19:18:02 -0700

    Testing automated git deployment

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">LRC Tutoring</h1>
+                <h1 className="display-3 mb-4">Git Test</h1>
                 <p className="lead">
                   {' '}
                   Get tutoring from the Learning Resource Center at CPP!
